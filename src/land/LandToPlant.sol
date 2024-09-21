e3a3aa21b37c3059bead343e0afef0fa492cb0be
pixotchi <pixotchi@proton.me>
2024-09-21 22:43:51 +0400

    Add access control to LandToPlant contract
    Integrated LandAccessControl to the LandToPlant contract to enforce security checks. Functions landToPlantAssignPlantPoints and landToPlantAssignLifeTime now use onlyAllowedCaller modifier to restrict access. This ensures that only authorized entities can call these functions, improving contract security.

--- src/land/LandToPlant.sol
+++ src/land/LandToPlant.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.26;
 
 import {LibLandToPlant} from "./LibLandToPlant.sol";
 import  {ILandToPlant} from "./ILandToPlant.sol";
+import  {LandAccessControl} from "./LandAccessControl.sol";
 
 /**
  * @title LandToPlant
@@ -11,16 +12,16 @@ import  {ILandToPlant} from "./ILandToPlant.sol";
  * @dev This contracts contains functions for calculating rewards, assigning points,
  *      and managing plant lifetimes
  */
-contract LandToPlant is ILandToPlant {
+contract LandToPlant is ILandToPlant, LandAccessControl {
 
-    function landToPlantAssignPlantPoints(uint256 _nftId, uint256 _addedPoints) external returns (uint256 _newPlantPoints) {
+    function landToPlantAssignPlantPoints(uint256 _nftId, uint256 _addedPoints) onlyAllowedCaller() external returns (uint256 _newPlantPoints)  {
         //TODO: security check. only land contract should be able to call this. 
         //TODO: we have to also make sure that the tx is form the owner.
         return LibLandToPlant.assignPlantPoints(_nftId, _addedPoints);
     }
 
 
-    function landToPlantAssignLifeTime(uint256 _nftId, uint256 _lifetime) external returns(uint256 _newLifetime) {
+    function landToPlantAssignLifeTime(uint256 _nftId, uint256 _lifetime) onlyAllowedCaller() external returns(uint256 _newLifetime) {
                 //TODO: security check. only land contract should be able to call this. 
         //TODO: we have to also make sure that the tx is form the owner.
         return LibLandToPlant.assignLifeTime(_nftId, _lifetime);
