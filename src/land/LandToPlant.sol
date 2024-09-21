d82ca58b9438d5e29472b224bb15f3d27befee6a
pixotchi <pixotchi@proton.me>
2024-09-21 18:54:14 +0400

    Update SPDX license, pragma version, and add plant-related functions
    Updated the SPDX license to MIT and increased the pragma Solidity version to 0.8.26. Introduced two new functions for assigning plant points and lifetimes, with placeholder security checks. Added documentation clarifying the contract's purpose.

--- src/land/LandToPlant.sol
+++ src/land/LandToPlant.sol
@@ -1,8 +1,28 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+import {LibLandToPlant} from "./LibLandToPlant.sol";
+
+/**
+ * @title LandToPlant
+ * @author 7118.eth
+ * @notice The extension ("facet") for managing plant-related operations in the game
+ * @dev This contracts contains functions for calculating rewards, assigning points,
+ *      and managing plant lifetimes
+ */
 contract LandToPlant {
-    constructor(){
 
+    function landToPlantAssignPlantPoints(uint256 _nftId, uint256 _addedPoints) external returns (uint256 _newPlantPoints) {
+        //TODO: security check. only land contract should be able to call this. 
+        //TODO: we have to also make sure that the tx is form the owner.
+        return LibLandToPlant.assignPlantPoints(_nftId, _addedPoints);
     }
+
+
+    function landToPlantAssignLifeTime(uint256 _nftId, uint256 _lifetime) external returns(uint256 _newLifetime) {
+                //TODO: security check. only land contract should be able to call this. 
+        //TODO: we have to also make sure that the tx is form the owner.
+        return LibLandToPlant.assignLifeTime(_nftId, _lifetime);
+    }
+
 }
